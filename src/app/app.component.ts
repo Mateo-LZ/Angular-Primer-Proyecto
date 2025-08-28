975c7aeff7c778b94f904d1c875b220a8a4ebf08
soy mateo <mateoleytonlopez01@gmail.com>
2025-08-28 08:42:40 -0500

    subiendo pagina angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'App';
+  
+  titulo = signal('TechStore');
+  descripcion = signal('Bienvenidos a TechStore tu tienda de confianza');
+  producto1 = signal({ nombre: 'Acer nitro 5 ', precio: 3000000, imagen: 'assets/Acer imagen.jpg' });
+  producto2 = signal({ nombre: 'Auriculares', precio: 350000, imagen: 'assets/audifonoscasco.jpeg' });
+  producto3 = signal({ nombre: 'Iphone', precio: 2800000, imagen: 'assets/iphone.webp' });
+
+  comprar (producto : string){
+    alert(`Has comprado: ${producto}`);
+  }
 }
